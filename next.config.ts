37128d971e410bd060a7b046fa8984e59f549104
Jhonata Vinicius <jhonyaraujo2488@gmail.com>
2026-04-18 21:26:02 -0300

    .

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,6 @@ const nextConfig: NextConfig = {
     additionalData: `@import "src/styles/variables.scss"; @import "src/styles/animations.scss";  @import "src/styles/mixins.scss";`,
   },
   swcMinify: true,
-  compress: true,                 // gzip responses for SSR (default true)
-  productionBrowserSourceMaps: false,
 };
 
 export default nextConfig;
